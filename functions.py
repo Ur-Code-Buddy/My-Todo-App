40597cae1706a5ae339cb1ef430673bcd7001fe1
Baivab <babai02dutta@gmail>
2023-05-03 20:36:54 +0530

    Initial commit

---
+++ functions.py
@@ -0,0 +1,17 @@
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
+    """Returns the files as read
+    """
+    with open(filepath, 'r') as file:
+        todos = file.readlines()
+    return todos
+
+
+def write_todos(todos_arg, filepath="todos.txt"):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("hello from todo")
